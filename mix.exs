7a88c8de23766f53d9ab1243ba7550436faa30f5
Jonathan Zanella <jonathanrafaelz@gmail.com>
2019-04-28 16:29:03 -0300

    Define elixir 1.8

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MyexpensesPhx.MixProject do
     [
       app: :myexpenses_phx,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
